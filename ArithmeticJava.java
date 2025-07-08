a02bd571ddee4e4de0be780dff45e5ed9f161b88
Shone-Kahuria <shonekahuria@gmail.com>
2025-07-08 11:46:45 +0300

    Refactor account hierarchy and implement DataSource interface with Admin, User, Delete, Update, and View classes; add exception handling in ArithmeticJava; create ArrayClear, ArrayReverse, Factorial, Forloop, OrderIdGenerator, Summation, and array classes.

--- ArithmeticJava.java
+++ ArithmeticJava.java
@@ -1,9 +1,16 @@
-class ArithmeticJava {
-public static void main(String[] args)
-{
-int number1 = 100;
-int number2 = 0;
-int answer = number1 / number2;
-System.out.println("Answer: " + answer);
-}
+
+public class ArithmeticJava {
+    public static void main(String[] args) {
+        int number1 = 300;
+        int number2 = 0;
+
+        try {
+            int ans = number1 / number2;
+            System.out.println("Answer: " + ans);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        } finally {
+            System.out.println("Program continues after handling the exception.");
+        }
+    }
 }
--- MainApplication.java
+++ MainApplication.java
@@ -4,7 +4,7 @@ public class MainApplication {
         Admin curAdmin = new Admin(1234, "Stephan", "xa1@LW42%3");
         User curUser = new User(4455, "John");
 
-        curAdmin.performOperation(new Update()); // Admin Updates the data.
-        curUser.performOperation(new View());    // User can view the data.
+        curAdmin.performOperation(new Update());
+        curUser.performOperation(new View());
     }
 }
